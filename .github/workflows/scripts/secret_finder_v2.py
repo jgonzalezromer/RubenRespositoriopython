e50e89e7e2a2770b5c56d881bf5e9bca0754ba39
jgonzalezromer <jgonzalezromero@danielcastelao.org>
2024-11-05 16:43:50 +0100

    Update secret_finder_v2.py

--- .github/workflows/scripts/secret_finder_v2.py
+++ .github/workflows/scripts/secret_finder_v2.py
@@ -5,10 +5,19 @@
 # Tip 1: Comando egrep de linux, método walk de la librería os, etc.
 # Tip 2: Códigos de salida con error para github
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/setting-exit-codes-for-actions
-
-
+def coincide_contraseña():
+    import subprocess
+    import sys
+    ruta=sys.argv[1]
+    palabra_clave=sys.argv[2]
+    resultado=subprocess.run([`egrep`,`-R`,`palabra_clave`,`ruta`], capture_outpo=true)
+    if resultado.return == 0
+        Print("Se ha encontrado una coincidencia")
+        ( sys.exit(150) )
+    else
+        ( sys.exit(0) )
 def main():
-    
+    coincide_contraseña()
     print("Todavía no estoy listo!")
 
 if __name__ == "__main__":
